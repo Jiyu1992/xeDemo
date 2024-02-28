205d234108b646e1aa89c9d5c073b970b9a026be
Lefteris Mantas <mantaslef@gmail.com>
2024-02-28 13:01:17 +0100

    Initial Commit

---
+++ xeDemoApp/xeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  xeDemoAppApp.swift
+//  xeDemoApp
+//
+//  Created by user256596 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct xeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
