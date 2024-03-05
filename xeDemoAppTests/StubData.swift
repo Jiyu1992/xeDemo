92b36feef4f365cc1ab6dc24f0af9671f2bc7fbb
Lefteris Mantas <mantaslef@gmail.com>
2024-03-05 13:46:16 +0100

    adding minimal unit testing

---
+++ xeDemoAppTests/StubData.swift
@@ -0,0 +1,23 @@
+//
+//  StubData.swift
+//  xeDemoAppTests
+//
+//  Created by user256596 on 3/5/24.
+//
+
+import Foundation
+
+public let stubData : Data = """
+[
+  {
+    "placeId": "1ee9a256c213540330ec272a9285f47912a30169",
+    "mainText": "Nafplio",
+    "secondaryText": "Ελλάδα"
+  },
+  {
+    "placeId": "e06e1bd03aa97bbec29c575ff5059252a92ca84c",
+    "mainText": "Nafpliou",
+    "secondaryText": "Αθήνα, Ελλάδα"
+  }
+]
+""".data(using: .utf8)!
--- xeDemoAppTests/xeDemoAppTests.swift
+++ xeDemoAppTests/xeDemoAppTests.swift
@@ -6,31 +6,32 @@
 //
 
 import XCTest
+import SwiftUI
 @testable import xeDemoApp
 
 final class xeDemoAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testClientDoesFetchAndDecodeData() async throws {
+        let apiClient = mockAPIClient()
+        let locations = try await apiClient.makeRequest(endpoint: Endpoints.getLocations(query: ""), responseModel: [LocationModel].self)
+        XCTAssertEqual(locations?.count, 2)
+    }
+    
+//  Here we can observe the drawbacks of foregoing MVVM. Testing the logic is kind of tricky because we moved it to the View layer.
+//  Considering the size and scope of this app this is completely fine but in a larger scale app we either follow the MVVM pattern
+//  and test the ViewModel OR we use a more SwiftUI-friendly architecture like TCA
+    @MainActor
+    func testSubmitButtonIsDeactivatedInInitialState() {
+//      As we manipulate the state variables INSIDE our View (instead of observing a VM or executing Actions of a Reducer in the TCA architecture)
+//      the code below represents the initial state of our App and we can NOT influence it directly
+        let view =  ContentView(locationProvider: LocationsProvider(apiClient: ApiClient()))
+        XCTAssertFalse(view.isLocationSelected && !view.formData.title.isEmpty)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+class mockAPIClient: ApiProtocol {
+    func makeRequest<T>(endpoint: xeDemoApp.EndpointProvider, responseModel: T.Type) async throws -> T? where T : Decodable {
+        try? await Task.sleep(nanoseconds: 3000)
+        return try JSONDecoder().decode(T.self, from: stubData)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
